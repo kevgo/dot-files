7a594920bbbbdf4ee8f327d2c5b4321745966610
Kevin Goslar <kevin.goslar@gmail.com>
2019-01-12 08:17:47 -0800

    Provide bang function in Fish

--- fish/config.fish
+++ fish/config.fish
@@ -83,10 +83,33 @@ function fish_title
   prompt_pwd
 end
 
+
+function bind_bang
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function bind_dollar
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+
 function fish_user_key_bindings
   bind \cv backward-kill-word
   bind \cb backward-word
   bind \cg accept-autosuggestion
+  bind ! bind_bang
+  bind '$' bind_dollar
 end
 
 function fish_prompt --description 'Write out the prompt'
