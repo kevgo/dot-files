f70e4671a2adfdb4b57b46e9eba1205cad41fac4
Kevin Goslar <kevin.goslar@gmail.com>
2020-05-21 20:11:26 -0500

    Remove Wine check

--- .config/git/git-prompt.sh
+++ .config/git/git-prompt.sh
@@ -4,19 +4,16 @@ PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
 PS1="$PS1"'\n'                 # new line
 PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
 PS1="$PS1"'\w'                 # current working directory
-if test -z "$WINELOADERNOEXEC"
+GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+if test -f "$COMPLETION_PATH/git-prompt.sh"
 then
-  GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
-  COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
-  COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
-  COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
-  if test -f "$COMPLETION_PATH/git-prompt.sh"
-  then
-    . "$COMPLETION_PATH/git-completion.bash"
-    . "$COMPLETION_PATH/git-prompt.sh"
-    PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-    PS1="$PS1"'`__git_ps1`'   # bash function
-  fi
+  . "$COMPLETION_PATH/git-completion.bash"
+  . "$COMPLETION_PATH/git-prompt.sh"
+  PS1="$PS1"'\[\033[36m\]'  # change color to cyan
+  PS1="$PS1"'`__git_ps1`'   # bash function
 fi
 PS1="$PS1"'\[\033[0m\]'        # change color
 PS1="$PS1"' > '             # prompt
