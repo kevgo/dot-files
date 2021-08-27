818ac674251b9755ef836156e4f9e0e0ee9f8259
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-27 16:26:13 -0500

    init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,100 +1,94 @@
 -- Leader key
-let g:mapleader = "\<space>"
-noremap \ ,
+vim.g.mapleader = " "
+--noremap \ ,
 
 
 -- Behavior
-set number                        -- show line numbers
-set autowriteall
-set nowritebackup                 -- don't create backup files
-set noswapfile                    -- don't create .swp files
-set ignorecase smartcase          -- ignore case in searched
-set noautoindent
-set nowrap                        -- disable word wrap.
-set history=200
-set scrolloff=45                  -- show context above/below cursorline
+vim.opt.number = true                        -- show line numbers
+vim.opt.autowriteall = true
+vim.opt.writebackup = false                -- don't create backup files
+vim.opt.swapfile = false                    -- don't create .swp files
+vim.opt.ignorecase = true
+vim.opt.smartcase = true          -- ignore case in searched
+vim.opt.autoindent = false
+vim.opt.wrap = false                      -- disable word wrap.
+vim.opt.history=200
+vim.opt.scrolloff=45                  -- show context above/below cursorline
 -- set cursorline
 -- set cursorcolumn
-set timeoutlen=500
-set title titlestring=%f          -- display filename in title bar
-set shell=/bin/bash
+vim.opt.timeoutlen=500
+vim.opt.title = true
+vim.opt.titlestring='%f'          -- display filename in title bar
+vim.opt.shell='/bin/bash'
 
 
 -- File explorer
-let g:netrw_banner = 0
+vim.g.netrw_banner = 0
 
 
 -- Fuzzy finder
-nnoremap <C-F> :FuzzyOpen "."<CR>
-nnoremap <leader>d :FuzzyOpen "."<CR>
+vim.api.nvim_set_keymap('n', '<C-F>', ':FuzzyOpen "."<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>d', ':FuzzyOpen "."<CR>', {noremap = true})
 
 
 -- NVimTree
-map <silent> <leader>t :NvimTreeToggle<CR>
-nnoremap <leader>n :NvimTreeFindFile<CR>
-let g:nvim_tree_follow = 1
-let g:nvim_tree_auto_close = 1
-let g:nvim_tree_show_icons = {
-    \ 'git': 0,
-    \ 'folders': 0,
-    \ 'files': 0,
-    \ 'folder_arrows': 0,
-    \ }
+vim.api.nvim_set_keymap('n', '<leader>t', ':NvimTreeToggle<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true})
+vim.g.nvim_tree_follow = 1
+vim.g.nvim_tree_auto_close = 1
+vim.g.nvim_tree_show_icons = { git = 0, folders = 0, files = 0, folder_arrows = 0 }
 
 
 -- Inserting empty lines using [enter] when in normal mode.
-map <S-Enter> O<Esc>
-map <CR> o<Esc>k
+vim.api.nvim_set_keymap('n', '<S-Enter>', 'O<Esc>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<CR>', 'o<Esc>k', {noremap = true})
 
 
 -- Insert spaces using space bar when in normal mode.
-nnoremap <silent> <leader><space> i <ESC>
+vim.api.nvim_set_keymap('n', '<leader><space>', 'i <ESC>', {noremap = true})
 
 
 -- Saving
 -- CTRL-S
-:noremap <c-s> <esc>:w<CR>
+vim.api.nvim_set_keymap('n', '<c-s>', '<esc>:w<CR>', {noremap = true})
 -- format on save
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost *.md,*.ts FormatWrite
-augroup END
+-- augroup FormatAutogroup
+--   autocmd!
+--   autocmd BufWritePost *.md,*.ts FormatWrite
+-- augroup END
 
 
 -- Search
-map <silent> <leader>/ :noh<CR>
+vim.api.nvim_set_keymap('n', '<leader>/', ':noh<CR>', {silent = true, noremap = true})
 
 
 -- Split window resizing
-nnoremap <silent> <Leader>= :exe "vertical resize +5"<CR>
-nnoremap <silent> <Leader>- :exe "vertical resize -5"<CR>
+vim.api.nvim_set_keymap('n', '<Leader>=', ':exe "vertical resize +5"<CR>', {silent = true, noremap = true})
+vim.api.nvim_set_keymap('n', '<Leader>-', ':exe "vertical resize -5"<CR>', {silent = true, noremap = true})
 
 
 -- Tabbing
-set softtabstop=2
-set shiftwidth=2
-set expandtab
-nnoremap <C-T> <esc>:tabnext<CR>
+vim.opt.softtabstop=2
+vim.opt.shiftwidth=2
+vim.opt.expandtab = true
+vim.api.nvim_set_keymap('n', '<C-T>', '<esc>:tabnext<CR>', {noremap = true})
 
 
--- Plug
-call plug#begin(stdpath('data') . '/plugged')
-
-Plug 'dag/vim-fish'
-Plug 'mhartington/formatter.nvim'
-Plug 'tomtom/tcomment_vim'
-Plug 'tpope/vim-markdown'
-Plug 'tpope/vim-repeat'
-Plug 'tpope/vim-surround'
-Plug 'cloudhead/neovim-fuzzy'
-Plug 'kyazdani42/nvim-tree.lua'
-
--- This must come after Plug
-call plug#end()
+-- Paq
+require "paq" {
+  "savq/paq-nvim";
+  'dag/vim-fish';
+  'mhartington/formatter.nvim';
+  'tomtom/tcomment_vim';
+  'tpope/vim-markdown';
+  'tpope/vim-repeat';
+  'tpope/vim-surround';
+  'cloudhead/neovim-fuzzy';
+  'kyazdani42/nvim-tree.lua';
+}
 
 
 -- Formatter
-lua << EOF
 require('formatter').setup({
   filetype = {
     markdown = {
@@ -108,27 +102,26 @@ require('formatter').setup({
     },
   }
 })
-EOF
 
 
 -- Yanking
-nmap Y y$
+vim.api.nvim_set_keymap('n', 'Y', 'y$', {noremap = true})
 
 
 -- DVORAK CUSTOMIZATIONS
 -- This has to be last for some reason.
 -- Cursor movement
-:nnoremap <silent> s l
-:nnoremap <silent> l s
-:vnoremap <silent> s l
-:vnoremap <silent> l s
--- cursor movement through panes using [leader]-[dvorak movement keys]
-nmap <silent> <leader>s <C-W>l
-nmap <silent> <leader>h <C-W>h
-nmap <silent> <leader>k <C-W>k
-nmap <silent> <leader>j <C-W>j
--- Moving panes using [leader]-[shift]-[dvorak movement keys]
-nmap <silent> <leader><S-S> <C-W>L
-nmap <silent> <leader><S-H> <C-W>H
-nmap <silent> <leader><S-K> <C-W>K
-nmap <silent> <leader><S-J> <C-W>J
+-- :nnoremap <silent> s l
+-- :nnoremap <silent> l s
+-- :vnoremap <silent> s l
+-- :vnoremap <silent> l s
+-- -- cursor movement through panes using [leader]-[dvorak movement keys]
+-- nmap <silent> <leader>s <C-W>l
+-- nmap <silent> <leader>h <C-W>h
+-- nmap <silent> <leader>k <C-W>k
+-- nmap <silent> <leader>j <C-W>j
+-- -- Moving panes using [leader]-[shift]-[dvorak movement keys]
+-- nmap <silent> <leader><S-S> <C-W>L
+-- nmap <silent> <leader><S-H> <C-W>H
+-- nmap <silent> <leader><S-K> <C-W>K
+-- nmap <silent> <leader><S-J> <C-W>J
