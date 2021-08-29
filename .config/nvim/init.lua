1a0ffc2736101eaf7e7a4d7225066fbe64d8c44b
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-29 10:45:01 -0500

    init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -27,82 +27,7 @@ o.titlestring='%f'                -- display filename in title bar
 o.shell='/bin/bash'
 
 
--- DVORAK CUSTOMIZATIONS
--- cursor movement inside panes
-map('n', 's', 'l', {silent = true, noremap = true})
-map('n', 'l', 's', {silent = true, noremap = true})
-map('v', 's', 'l', {silent = true, noremap = true})
-map('v', 'l', 's', {silent = true, noremap = true})
--- cursor movement through panes using [leader]-[dvorak movement keys]
-map('n', '<leader>s', '<C-W>l', {silent = true, noremap = true})
-map('n', '<leader>h', '<C-W>h', {silent = true, noremap = true})
-map('n', '<leader>k', '<C-W>k', {silent = true, noremap = true})
-map('n', '<leader>j', '<C-W>j', {silent = true, noremap = true})
--- moving panes using [leader]-[shift]-[dvorak movement keys]
-map('n', '<leader><S-S>', '<C-W>L', {silent = true, noremap = true})
-map('n', '<leader><S-H>', '<C-W>H', {silent = true, noremap = true})
-map('n', '<leader><S-K>', '<C-W>K', {silent = true, noremap = true})
-map('n', '<leader><S-J>', '<C-W>J', {silent = true, noremap = true})
-
-
--- file explorer
-g.netrw_banner = 0
-
-
--- fuzzy finder
-map('n', '<C-F>', ':FuzzyOpen "."<CR>', {silent = true, noremap = true})
-map('n', '<leader>d', ':FuzzyOpen "."<CR>', {silent = true, noremap = true})
-
-
--- NVimTree
-map('n', '<leader>t', ':NvimTreeToggle<CR>', {silent = true, noremap = true})
-g.nvim_tree_follow = 1
-g.nvim_tree_auto_close = 1
-g.nvim_tree_show_icons = { git = 0, folders = 0, files = 0, folder_arrows = 0 }
-
-
--- inserting empty lines using [enter] when in normal mode.
-map('n', '<S-Enter>', 'O<Esc>', {silent = true, noremap = true})
-map('n', '<CR>', 'o<Esc>k', {silent = true, noremap = true})
-
-
--- insert spaces using space bar when in normal mode.
-map('n', '<leader><space>', 'i <ESC>', {silent = true, noremap = true})
-
-
--- saving
--- CTRL-S
-map('n', '<c-s>', '<esc>:w<CR>', {silent = true, noremap = true})
--- format on save
--- TODO: use the native bindings once https://github.com/neovim/neovim/pull/14661 ships
-cmd('augroup FormatAutogroup')
-cmd('autocmd!')
-cmd('autocmd BufWritePost *.md,*.ts FormatWrite')
-cmd('augroup END')
-
-
--- search
-map('n', '<leader>/', ':noh<CR>', {silent = true, noremap = true})
-map('n', '<leader>n', ':noh<CR>', {silent = true, noremap = true})
-
-
--- split window resizing
-map('n', '<Leader>=', ':exe "vertical resize +5"<CR>', {silent = true, noremap = true})
-map('n', '<Leader>-', ':exe "vertical resize -5"<CR>', {silent = true, noremap = true})
-
-
--- tabbing
-o.softtabstop=2
-o.shiftwidth=2
-o.expandtab = true
-map('n', '<C-T>', '<esc>:tabnext<CR>', {noremap = true})
-
-
--- yanking
-map('n', 'Y', 'y$', {noremap = true})
-
-
--- paq
+-- plugins
 require 'paq' {
   'savq/paq-nvim';
   'dag/vim-fish';
@@ -130,3 +55,81 @@ require('formatter').setup({
     },
   }
 })
+
+-- colors
+cmd("highlight Search ctermbg=green ctermfg=16")  -- make search results readable
+cmd("highlight Todo ctermbg=blue ctermfg=16")  -- make TODO readable
+
+
+
+-- DVORAK CUSTOMIZATIONS
+-- cursor movement inside panes
+map('n', 's', 'l', {silent = true, noremap = true})
+map('n', 'l', 's', {silent = true, noremap = true})
+map('v', 's', 'l', {silent = true, noremap = true})
+map('v', 'l', 's', {silent = true, noremap = true})
+-- cursor movement through panes using [leader]-[dvorak movement keys]
+map('n', '<leader>s', '<C-W>l', {silent = true, noremap = true})
+map('n', '<leader>h', '<C-W>h', {silent = true, noremap = true})
+map('n', '<leader>k', '<C-W>k', {silent = true, noremap = true})
+map('n', '<leader>j', '<C-W>j', {silent = true, noremap = true})
+-- moving panes using [leader]-[shift]-[dvorak movement keys]
+map('n', '<leader><S-S>', '<C-W>L', {silent = true, noremap = true})
+map('n', '<leader><S-H>', '<C-W>H', {silent = true, noremap = true})
+map('n', '<leader><S-K>', '<C-W>K', {silent = true, noremap = true})
+map('n', '<leader><S-J>', '<C-W>J', {silent = true, noremap = true})
+
+
+-- fuzzy finder
+map('n', '<C-F>', ':FuzzyOpen "."<CR>', {silent = true, noremap = true})
+map('n', '<leader>d', ':FuzzyOpen "."<CR>', {silent = true, noremap = true})
+
+
+-- nvimtree
+map('n', '<leader>t', ':NvimTreeToggle<CR>', {silent = true, noremap = true})
+g.nvim_tree_follow = 1
+g.nvim_tree_auto_close = 1
+g.nvim_tree_show_icons = { git = 0, folders = 0, files = 0, folder_arrows = 0 }
+
+
+-- inserting empty lines using [enter] when in normal mode.
+map('n', '<S-Enter>', 'O<Esc>', {silent = true, noremap = true})
+map('n', '<CR>', 'o<Esc>k', {silent = true, noremap = true})
+
+
+-- insert spaces using space bar when in normal mode.
+map('n', '<leader><space>', 'i <ESC>', {silent = true, noremap = true})
+
+
+-- saving
+-- CTRL-S
+map('n', '<c-s>', '<esc>:w<CR>', {silent = true, noremap = true})
+-- format on save
+-- TODO: use the native Lua bindings once https://github.com/neovim/neovim/pull/14661 ships
+cmd('augroup FormatAutogroup')
+cmd('autocmd!')
+cmd('autocmd BufWritePost *.md,*.ts FormatWrite')
+cmd('augroup END')
+
+
+-- search
+map('n', '<leader>/', ':noh<CR>', {silent = true, noremap = true})
+map('n', '<leader>n', ':noh<CR>', {silent = true, noremap = true})
+
+
+-- split window resizing
+map('n', '<Leader>=', ':exe "vertical resize +5"<CR>', {silent = true, noremap = true})
+map('n', '<Leader>-', ':exe "vertical resize -5"<CR>', {silent = true, noremap = true})
+
+
+-- tabbing
+o.softtabstop=2
+o.shiftwidth=2
+o.expandtab = true
+map('n', '<C-T>', '<esc>:tabnext<CR>', {noremap = true})
+
+
+-- yanking
+map('n', 'Y', 'y$', {noremap = true})
+
+
