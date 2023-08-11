6d90afa80ada16635f7fb0b41b6bca397ccbe976
Kevin Goslar <kevin.goslar@gmail.com>
2023-08-10 19:57:10 -0500

    extensions.json keybindings.json settings.json javascript.json rust.json typescript.json extensions.json keybindings.json settings.json javascript.json rust.json typescript.json settings.json config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -177,6 +177,10 @@ function gd
   git diff 
 end
 
+function gdc
+  git diff --word-diff-regex=.
+end
+
 function gdw
   git diff --color-words 
 end
@@ -311,10 +315,20 @@ abbr -ag pg pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.
 abbr -ag pg_stop pg_ctl -D /usr/local/var/postgres stop
 
 
+# Python
+if test -f /opt/homebrew/anaconda3/bin/conda
+    eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# if test -d /opt/homebrew/anaconda3/bin
+#   set -x PATH /opt/homebrew/anaconda3/bin $PATH
+#   conda init fish
+# end
+
 # Rancher Desktop
 if test -d ~/.rd/bin
   set -x PATH $PATH ~/.rd/bin
 end
+
 
 # Ruby
 if test -d ~/.rbenv
