1e30967801fc8d216e20917a4fd49e73c72b71e0
Kevin Goslar <kevin.goslar@gmail.com>
2024-08-24 09:29:52 -0500

    config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -139,7 +139,7 @@ function fish_prompt --description 'Write out the prompt'
   else
     set __yellow_pending_gittown_command ''
   end
-  printf '%s%s%s%s > ' $__green_path $__blue_git_branch $__yellow_pending_gittown_command $__red_last_status 
+  printf '%s%s%s%s > ' $__green_path $__blue_git_branch $__yellow_pending_gittown_command $__red_last_status
 end
 
 
